6d3e10db3a50f6cf467add8630f97ee847d085cb
Programmer rabbit <154576550+NayemyMurilloRivera@users.noreply.github.com>
2026-06-16 16:36:15 -0500

    Create LECCION8_MAXMINHEAP_NODOS.cpp

---
+++ LECCION8_MAXMINHEAP_NODOS.cpp
@@ -0,0 +1,1 @@
+//Implementar una clase CHeap<T,Ord> donde T es el tipo de dato y Ord es un function-object que determina el orden del heap (MaxHeap y MinHeap). Implementar esta idea utilizando nodos y punteros.
